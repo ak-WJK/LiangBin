87f790a57e463952325b1b9dc253fbcbd3902029
ak-WJK <WJK2288@163.com>
2017-07-22 16:10:32 +0800

    支付完成

--- app/src/main/java/com/wjk2288/liangbin/activity/activity/MainActivity.java
+++ app/src/main/java/com/wjk2288/liangbin/activity/activity/MainActivity.java
@@ -255,4 +255,5 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
 }
--- app/src/main/java/com/wjk2288/liangbin/activity/shop/fragment/SelfFragment.java
+++ app/src/main/java/com/wjk2288/liangbin/activity/shop/fragment/SelfFragment.java
@@ -1,10 +1,29 @@
 package com.wjk2288.liangbin.activity.shop.fragment;
 
-import android.graphics.Color;
-import android.view.View;
-import android.widget.TextView;
 
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.wjk2288.liangbin.R;
+import com.wjk2288.liangbin.activity.activity.MainActivity;
 import com.wjk2288.liangbin.activity.shop.base.BaseFragment;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+//import android.support.v7.app.ActionBarDrawerToggle;
+//import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.widget.Toolbar;
+;
 
 /**
  * Created by Administrator on 2017/7/6.
@@ -12,17 +31,55 @@ import com.wjk2288.liangbin.activity.shop.base.BaseFragment;
 
 public class SelfFragment extends BaseFragment {
 
-    private TextView textView;
 
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+    @Bind(R.id.nav_view)
+    NavigationView navView;
+    @Bind(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     public View initView() {
-        textView = new TextView(context);
-        textView.setTextColor(Color.BLACK);
-        return textView;
+        View view = LayoutInflater.from(context).inflate(R.layout.fragment_self, null);
+        ButterKnife.bind(this, view);
+
+        MainActivity mainActivity = (MainActivity) context;
+        mainActivity.setSupportActionBar(toolbar);
+
+        final ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(mainActivity,
+                drawerLayout,
+                toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
+        );
+        drawerToggle.syncState();
+
+        //设置导航视图的点击事件
+
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+
+        });
+
+        return view;
     }
 
     @Override
     protected void initData() {
-        textView.setText("个人");
+
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
     }
 }
